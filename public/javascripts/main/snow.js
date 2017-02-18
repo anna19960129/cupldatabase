979439b1e4c6a0d6487f5b23b4f615d8de6b8b22
snow118 <wangbaixue118@163.com>
2017-02-18 11:07:38 +0800

    snow1

--- public/javascripts/main/snow.js
+++ public/javascripts/main/snow.js
@@ -1,3 +1,11 @@
 /**
  * Created by samsung2014 on 2017/2/17.
  */
+function display_cpt1() {
+    document.getElementById("cpttext1").style.display = "block";
+    document.getElementById("cpttext2").style.display = "none";
+}
+function display_cpt2() {
+    document.getElementById("cpttext2").style.display = "block";
+    document.getElementById("cpttext1").style.display = "none";
+}
