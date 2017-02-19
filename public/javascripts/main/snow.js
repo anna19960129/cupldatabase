3fbe39a396de1502cda9a5774ed78c717a0dbecb
hugowangguowei <wang876856259@126.com>
2017-02-19 11:51:35 +0800

    snow 代码review

--- public/javascripts/main/snow.js
+++ public/javascripts/main/snow.js
@@ -29,18 +29,30 @@ function display_cpt2() {
     }
 }
 var cptCount=1;
+
 function cpt_add() {
     var element = document.createElement("tr");
-    var node = document.createTextNode("其他组员");
-    element.appendChild(node);
     element.setAttribute('class', "cptOtherMember");
+    //表格的一行还包括了两列，所以必须要把列结构也添上
+    //如果没有td的话，所有的内容都会挤占在默认的第一列
+    //会让你感觉到都在左边
+    var td1 = document.createElement("td");
+    var node = document.createTextNode("其他组员");
+    td1.appendChild(node);
+    var td2 = document.createElement("td");
     var inp = document.createElement("input");
     inp.setAttribute("type", "text");
     cptCount++;
     inp.id='cpt_member'+ paperCount;
-    element.appendChild(inp);
-    x = document.getElementById("cptInfoTable");
-    x.appendChild(element);
+    td2.appendChild(inp);
+
+    element.appendChild(td1);
+    element.appendChild(td2);
+
+    //这里指定了在table的最后一行前面添加一行
+    var table = document.getElementById("cptInfoTable");
+    var lastLine = document.getElementById("cptInfoTableButton");
+    table.children[0].insertBefore(element,lastLine);
 }
 
 function cpt_del() {
